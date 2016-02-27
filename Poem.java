6b311ec83af1212afca7e40dcedafbb4a0a4a5e9
PhoebeChen115 <102316237@gms.tcu.edu.tw>
2016-02-27 23:23:57 +0800

    homework1
    七言絕句

---
+++ Poem.java
@@ -0,0 +1,8 @@
+public class Poem {
+  public static void main(String[] args)  {
+    System.out.println("日照香炉生紫烟,\n");
+	System.out.println("遥看瀑布挂前川。\n");
+	System.out.println("飞流直下三千尺，\n");
+	System.out.println("疑是银河落九天。\n");
+  }
+}
